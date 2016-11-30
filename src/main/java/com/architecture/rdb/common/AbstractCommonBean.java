31f848c60e497e5f3d992ef4ba0d8655ed700674
Daniel Menezes <danielmenezes@MacBook-Pro-de-Daniel.local>
2016-11-30 19:05:03 -0200

    Correcao

--- src/main/java/com/architecture/rdb/common/AbstractCommonBean.java
+++ src/main/java/com/architecture/rdb/common/AbstractCommonBean.java
@@ -11,18 +11,15 @@ public abstract class AbstractCommonBean implements CommonBean {
 	/** Generated Serial UID Version. */
 	private static final long serialVersionUID = -2050248527788932359L;
 	
-	/** Atributo de identificação do bean. */
-	protected Integer id;
-
 	/**
 	 * Realiza a comparação básica sobre a identificação do bean.
 	 * @param bean Bean a ser comparado
 	 */
 	public int compareTo(AbstractCommonBean bean) {
-		if(this.id == null || bean.getId() == null) {
+		if(this.getId() == null || bean.getId() == null) {
 			return 1;
 		}
-		return id.compareTo(bean.getId());
+		return getId().compareTo(bean.getId());
 	}
 
 	/**
@@ -32,7 +29,7 @@ public abstract class AbstractCommonBean implements CommonBean {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
 		return result;
 	}
 	
@@ -51,11 +48,11 @@ public abstract class AbstractCommonBean implements CommonBean {
 			return false;
 		}
 		AbstractCommonBean other = (AbstractCommonBean) obj;
-		if (id == null) {
-			if (other.id != null) {
+		if (getId() == null) {
+			if (other.getId() != null) {
 				return false;
 			}
-		} else if (!id.equals(other.id)) {
+		} else if (!getId().equals(other.getId())) {
 			return false;
 		}
 		return true;
